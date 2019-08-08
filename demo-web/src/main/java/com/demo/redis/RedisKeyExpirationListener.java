c8d1c9c0e85f46c058a7e2cd2494a4af63ae5bd9
wanghouji <wanghouji@ruifucredit.com>
2019-08-08 17:01:08 +0800

    redis单机模式，过期key的事件

--- demo-web/src/main/java/com/demo/redis/RedisKeyExpirationListener.java
+++ demo-web/src/main/java/com/demo/redis/RedisKeyExpirationListener.java
@@ -1,31 +1,31 @@
-package com.demo.redis;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.connection.Message;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.KeyExpirationEventMessageListener;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.stereotype.Component;
-
-/**
- * 监听所有reids的过期事件,仅适合单机！！！！！！！！！！！！！
- */
-@Component
-public class RedisKeyExpirationListener extends KeyExpirationEventMessageListener {
-    
-    @Autowired
-    private RedisTemplate redisTemplate;
- 
-    public RedisKeyExpirationListener(RedisMessageListenerContainer listenerContainer) {
-        super(listenerContainer);
-    }
- 
- 
-    @Override
-    public void onMessage(Message message, byte[] pattern) {
- 
-        String expiredKey = message.toString();
- 
-        System.out.println("过期的key 是: "+expiredKey);
-    }
-}
+//package com.demo.redis;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.redis.connection.Message;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.listener.KeyExpirationEventMessageListener;
+//import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * 监听所有reids的过期事件,仅适合单机！！！！！！！！！！！！！
+// */
+//@Component
+//public class RedisKeyExpirationListener extends KeyExpirationEventMessageListener {
+//    
+//    @Autowired
+//    private RedisTemplate redisTemplate;
+// 
+//    public RedisKeyExpirationListener(RedisMessageListenerContainer listenerContainer) {
+//        super(listenerContainer);
+//    }
+// 
+// 
+//    @Override
+//    public void onMessage(Message message, byte[] pattern) {
+// 
+//        String expiredKey = message.toString();
+// 
+//        System.out.println("过期的key 是: "+expiredKey);
+//    }
+//}
