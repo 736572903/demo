e31b81f4dbb2cc152fa57af46e7a66926b399fe0
wanghouji <wanghouji@wecash.net>
2019-11-04 10:49:00 +0800

    格式化

--- demo-web/src/main/java/com/demo/startup/DemoApplication.java
+++ demo-web/src/main/java/com/demo/startup/DemoApplication.java
@@ -6,9 +6,15 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ * 功能描述: 启动类
+ *
+ * @author wanghouji
+ * @date 2019/11/4 10:48 上午
+ */
 @SpringBootApplication
-@EnableTransactionManagement//事务
-@EnableAspectJAutoProxy//代理
+@EnableTransactionManagement
+@EnableAspectJAutoProxy
 @ComponentScan(basePackages = {"com.demo"})
 public class DemoApplication {
 
